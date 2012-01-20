13c2b2d70477ef9f7531e3eef93857c57e5745e5
John Griffin <johnog@gmail.com>
2012-01-20 16:37:23 +0000

    cache hits to the knowyourmeme site - may need tweaking to work on heroku

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ gem "nokogiri", "~> 1.5.0"
 gem 'faraday', "~> 0.7.5"
 gem 'faraday-stack'
 gem 'json'
+gem "i18n"
+gem "activesupport"
 
 group :development do
   gem 'shotgun'
--- watme.rb
+++ watme.rb
@@ -1,10 +1,14 @@
 require "rubygems"
 require "bundler/setup"
 Bundler.require(:default)
+require "active_support/cache"
 
 def wats
   connection = Faraday.new(:url => 'http://knowyourmeme.com/memes/wat/photos?sort=score') do |builder|
     builder.use FaradayStack::ResponseHTML
+    builder.use FaradayStack::Caching do
+      ActiveSupport::Cache::FileStore.new 'tmp/cache', :namespace => 'faraday', :expires_in => 3600
+    end
     builder.adapter Faraday.default_adapter
   end
   response = connection.get
